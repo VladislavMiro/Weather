da6453b4dd089d5c25f523749dd06966b892f455
Vladislav Miroshnichenko <vladislavmiro@icloud.com>
2023-12-09 05:04:27 +0300

    Add Resources for appplication

---
+++ Weather/Core/Resources/Resources/Resources.swift
@@ -0,0 +1,29 @@
+//
+//  Resources.swift
+//  AstonWeather
+//
+//  Created by Vladislav Miroshnichenko on 01.12.2023.
+//
+
+import UIKit
+
+enum Resources {
+    
+    struct Colors {
+        static let backgroundColor = UIColor(named: "BackgroundColor")
+        static let secondBackgroundColor = UIColor(named: "SecondBackgroundColor")
+        static let fontColor = UIColor(named: "FontColor")
+        static let secondFontColor = UIColor(named: "SecondFontColor")
+    }
+    
+    struct Symbols {
+        static let celciusSymbol = "°"
+        static let kmPerHour = " km/h"
+        static let km = " km"
+        static let precent = " %"
+        static let hPa = " hPa"
+        static let mBa = " mBa"
+        static let mm = " mm"
+    }
+    
+}
