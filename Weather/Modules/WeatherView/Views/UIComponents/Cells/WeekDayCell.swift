e9c87df4ddcaf7cddb840695eab9562c9693deb5
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2026-04-03 22:44:05 +0300

    SnapKit for WeekDayCell has been added. Code refactoring.

--- Weather/Modules/WeatherView/Views/UIComponents/Cells/WeekDayCell.swift
+++ Weather/Modules/WeatherView/Views/UIComponents/Cells/WeekDayCell.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
 final class WeekDayCell: UITableViewCell {
 
-    //MARK: - Public fields
+    //MARK: - Public properties
     
-    public static let cellIdentifier = "WeekDayCell"
+    public static let cellIdentifier = StringConstants.cellIdentifier
     
-    //MARK: - Private fields
+    //MARK: - Private properties
     
     private let imageStack: UIStackView = {
         let view = UIStackView()
@@ -21,7 +22,7 @@ final class WeekDayCell: UITableViewCell {
         view.axis = .horizontal
         view.alignment = .center
         view.distribution = .fill
-        view.spacing = 5
+        view.spacing = LayoutConstants.imageStackSpacing
         
         return view
     }()
@@ -32,7 +33,7 @@ final class WeekDayCell: UITableViewCell {
         view.axis = .horizontal
         view.distribution = .fill
         view.alignment = .fill
-        view.spacing = 5
+        view.spacing = LayoutConstants.stackViewSpacing
         view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
@@ -41,7 +42,7 @@ final class WeekDayCell: UITableViewCell {
     private let dayLabel: UILabel = {
         let label = UILabel()
     
-        label.font = .systemFont(ofSize: 14)
+        label.font = Fonts.dayLabel
         label.textColor = Resources.Colors.secondFontColor
         label.adjustsFontSizeToFitWidth = true
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -52,8 +53,8 @@ final class WeekDayCell: UITableViewCell {
     private let temperatureLabel: UILabel = {
         let label = UILabel()
         
-        label.font = .systemFont(ofSize: 14)
-        label.numberOfLines = 1
+        label.font = Fonts.temperatureLabel
+        label.numberOfLines = Constants.numberOfLines
         label.textAlignment = .right
         label.textColor = Resources.Colors.secondFontColor
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -64,8 +65,8 @@ final class WeekDayCell: UITableViewCell {
     private let conditionLabel: UILabel = {
         let label = UILabel()
         
-        label.font = .systemFont(ofSize: 14)
-        label.numberOfLines = 1
+        label.font = Fonts.conditionLabel
+        label.numberOfLines = Constants.numberOfLines
         label.textAlignment = .left
         label.adjustsFontForContentSizeCategory = true
         label.adjustsFontSizeToFitWidth = true
@@ -96,18 +97,26 @@ final class WeekDayCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Public methods
+}
+
+//MARK: - Extension with public methods
+
+extension WeekDayCell {
     
     public func setData(data: WeatherViewDataModels.WeekForecastOutputData) {
         self.dayLabel.text = data.day
         self.conditionLabel.text = data.condition
         self.temperatureLabel.text = data.temperature
-        self.image.image = UIImage(named: data.icon) 
+        self.image.image = UIImage(named: data.icon)
     }
     
-    //MARK: - Private methods
+}
+
+//MARK: - Extension with private methods
+
+private extension WeekDayCell {
     
-    private func configuration() {
+    func configuration() {
         backgroundColor = Resources.Colors.secondBackgroundColor
         
         imageStack.addArrangedSubview(image)
@@ -120,19 +129,52 @@ final class WeekDayCell: UITableViewCell {
         contentView.addSubview(stackView)
     }
     
-    private func constraints() {
-        NSLayoutConstraint.activate([
-            dayLabel.widthAnchor.constraint(equalToConstant: 80),
-            temperatureLabel.widthAnchor.constraint(equalToConstant: 50),
-                        
-            image.heightAnchor.constraint(equalToConstant: 32),
-            image.widthAnchor.constraint(equalTo: image.heightAnchor),
-            
-            stackView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor)
-        ])
+    func constraints() {
+        
+        dayLabel.snp.makeConstraints {
+            $0.width.equalTo(LayoutConstants.dayLabelWidth)
+        }
+        
+        temperatureLabel.snp.makeConstraints {
+            $0.width.equalTo(LayoutConstants.temperatureLabelWidth)
+        }
+        
+        image.snp.makeConstraints {
+            $0.size.equalTo(LayoutConstants.imageViewSize)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(contentView.layoutMarginsGuide.snp.edges)
+        }
+        
+    }
+    
+}
+
+//MARK: - Extension with private subobjects
+
+private extension WeekDayCell {
+    
+    enum LayoutConstants {
+        static let dayLabelWidth: CGFloat = 80.0
+        static let temperatureLabelWidth: CGFloat = 50.0
+        static let imageViewSize: CGFloat = 32.0
+        static let imageStackSpacing: CGFloat = 5.0
+        static let stackViewSpacing: CGFloat = 5.0
+    }
+    
+    enum Constants {
+        static let numberOfLines: Int = 1
+    }
+    
+    enum Fonts {
+        static let dayLabel: UIFont = .systemFont(ofSize: 14)
+        static let temperatureLabel: UIFont = .systemFont(ofSize: 14)
+        static let conditionLabel: UIFont = .systemFont(ofSize: 14)
+    }
+    
+    enum StringConstants {
+        static let cellIdentifier: String = String(describing: WeekDayCell.self)
     }
     
 }
