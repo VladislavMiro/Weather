1e30ab608c82d6407122a0da1f54c617d77f4a08
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2026-04-03 21:27:27 +0300

    SnapKit for DayForecastView has been added. Code refactoring.

--- Weather/Modules/WeatherView/Views/UIComponents/DayForecastView.swift
+++ Weather/Modules/WeatherView/Views/UIComponents/DayForecastView.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 import Combine
+import SnapKit
 
 final class DayForecastView: UIView {
 
-    //MARK: - Private fields
+    //MARK: - Private properties
     
     private let label: UILabel = {
         let label = UILabel()
         
-        label.text = "Day Forecast"
+        label.text = StringConstants.label
         label.textColor = Resources.Colors.secondFontColor
         label.textAlignment = .left
-        label.font = .boldSystemFont(ofSize: 16)
+        label.font = Fonts.title
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -28,8 +29,12 @@ final class DayForecastView: UIView {
         let view = UICollectionView(frame: .zero, collectionViewLayout: .init())
         let layout = UICollectionViewFlowLayout()
         
-        layout.sectionInset = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
-        layout.itemSize = CGSize(width: 115, height: 115)
+        layout.sectionInset = UIEdgeInsets(top: LayoutConstants.sectionVerticalInset,
+                                           left: LayoutConstants.sectionHorizontalInset,
+                                           bottom: LayoutConstants.sectionVerticalInset,
+                                           right: LayoutConstants.sectionHorizontalInset)
+        layout.itemSize = CGSize(width: LayoutConstants.collectionViewItemSize,
+                                 height: LayoutConstants.collectionViewItemSize)
         layout.scrollDirection = .horizontal
         
         view.collectionViewLayout = layout
@@ -64,42 +69,9 @@ final class DayForecastView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Private methods
-    
-    private func configure() {
-        backgroundColor = Resources.Colors.secondBackgroundColor
-        layer.cornerRadius = 15
-        self.clipsToBounds = true
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        addSubview(label)
-        addSubview(collectionView)
-    }
-    
-    private func bind() {
-        viewModel.refreshData.sink { [unowned self] _ in
-            self.collectionView.reloadData()
-        }.store(in: &cancelable)
-    }
-    
-    private func constraints() {
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor, constant: 5),
-            label.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor, constant: 15),
-            label.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
-            
-            collectionView.topAnchor.constraint(equalTo: label.layoutMarginsGuide.bottomAnchor, constant: 15),
-            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            collectionView.heightAnchor.constraint(equalToConstant: 150),
-            collectionView.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor),
-            
-            self.heightAnchor.constraint(equalToConstant: 200)
-        ])
-    }
 }
+
+//MARK: - Extension with UICollectionViewDelegate & UICollectionViewDataSource implementations
 
 extension DayForecastView: UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -115,6 +87,80 @@ extension DayForecastView: UICollectionViewDelegate, UICollectionViewDataSource 
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.output.count
+    }
+    
+}
+
+//MARK: - Extension with private methods
+
+private extension DayForecastView {
+    
+    func configure() {
+        backgroundColor = Resources.Colors.secondBackgroundColor
+        layer.cornerRadius = LayoutConstants.superViewCornerRadius
+        self.clipsToBounds = true
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        addSubview(label)
+        addSubview(collectionView)
+    }
+    
+    func bind() {
+        viewModel.refreshData.sink { [unowned self] _ in
+            self.collectionView.reloadData()
+        }.store(in: &cancelable)
+    }
+    
+    func constraints() {
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(LayoutConstants.superViewHeight)
+        }
+        
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview()
+                .offset(LayoutConstants.labelTopOffset)
+            $0.leading.equalToSuperview()
+                .offset(LayoutConstants.labelLeadingOffset)
+            $0.trailing.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom)
+                .offset(LayoutConstants.collectionViewTopOffset)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(LayoutConstants.collectionViewHeight)
+        }
+
+    }
+    
+}
+
+//MARK: - Extension with private subobjects
+
+private extension DayForecastView {
+    
+    enum LayoutConstants {
+        static let labelTopOffset: CGFloat = 5.0
+        static let labelLeadingOffset: CGFloat = 15.0
+        static let collectionViewTopOffset: CGFloat = 15.0
+        static let collectionViewHeight: CGFloat = 150.0
+        static let superViewHeight: CGFloat = 200.0
+        static let superViewCornerRadius: CGFloat = 15.0
+        static let sectionVerticalInset: CGFloat = 10.0
+        static let sectionHorizontalInset: CGFloat = 15.0
+        static let collectionViewItemSize: CGFloat = 115.0
+    }
+    
+    enum Fonts {
+        static let title: UIFont = .boldSystemFont(ofSize: 16)
+    }
+    
+    enum StringConstants {
+        static let label: String = "Day Forecast"
     }
     
 }
