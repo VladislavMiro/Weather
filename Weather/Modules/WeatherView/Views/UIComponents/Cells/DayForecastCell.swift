f7c2758225b216e879c61b7197957adc12eb15e4
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2026-04-03 22:27:31 +0300

    DayForecastCell's code refactoring.

--- Weather/Modules/WeatherView/Views/UIComponents/Cells/DayForecastCell.swift
+++ Weather/Modules/WeatherView/Views/UIComponents/Cells/DayForecastCell.swift
@@ -10,13 +10,13 @@ import Combine
 
 final class DayForecastCell: UICollectionViewCell {
     
-    //MARK: - Public fields
+    //MARK: - Public properties
     
     typealias OutputData = WeatherViewDataModels.DayForecastCellOutputData
     
-    public static let cellIdentifier = "DayForecastCell"
+    public static let cellIdentifier = StringConstants.cellIdentifier
     
-    //MARK: - Private fields
+    //MARK: - Private properties
     
     private let stackView: UIStackView =  {
         let view = UIStackView()
@@ -24,7 +24,7 @@ final class DayForecastCell: UICollectionViewCell {
         view.axis = .vertical
         view.distribution = .fillEqually
         view.alignment = .center
-        view.spacing = 5
+        view.spacing = LayoutConstants.stackViewSpacing
         
         return view
     }()
@@ -32,7 +32,7 @@ final class DayForecastCell: UICollectionViewCell {
     private let timeLabel: UILabel = {
         let label = UILabel()
         
-        label.font = .boldSystemFont(ofSize: 14)
+        label.font = Fonts.timeLabel
         label.textColor = Resources.Colors.secondFontColor
         label.textAlignment = .center
         
@@ -42,7 +42,7 @@ final class DayForecastCell: UICollectionViewCell {
     private let tempLabel: UILabel = {
         let label = UILabel()
         
-        label.font = .boldSystemFont(ofSize: 14)
+        label.font = Fonts.tempLabel
         label.textColor = Resources.Colors.secondFontColor
         label.textAlignment = .center
         
@@ -50,7 +50,9 @@ final class DayForecastCell: UICollectionViewCell {
     }()
     
     private let imageView: UIImageView = {
-        let view = UIImageView(frame: .init(origin: .zero, size: .init(width: 28, height: 28)))
+        let imageSize = CGSize(width: LayoutConstants.imageViewSize,
+                               height: LayoutConstants.imageViewSize)
+        let view = UIImageView(frame: .init(origin: .zero, size: imageSize))
     
         view.contentMode = .scaleAspectFill
         
@@ -68,19 +70,27 @@ final class DayForecastCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Public Methods
-    
+}
+
+//MARK: - Extension with public methods
+
+extension DayForecastCell {
+
     public func setup(data: OutputData) {
         self.timeLabel.text = data.time
         self.tempLabel.text = data.temperature
         self.imageView.image = UIImage(named: data.icon)
     }
     
-    //MARK: - Private methods
+}
+
+//MARK: - Extension with private methods
+
+private extension DayForecastCell {
     
-    private func configure() {
+    func configure() {
         backgroundColor = Resources.Colors.backgroundColor
-        layer.cornerRadius = 15
+        layer.cornerRadius = LayoutConstants.superViewCornerRadius
     
         stackView.frame = self.bounds
         
@@ -89,6 +99,27 @@ final class DayForecastCell: UICollectionViewCell {
         stackView.addArrangedSubview(tempLabel)
         
         addSubview(stackView)
+    }
+    
+}
+
+//MARK: - Extension with private subobjects
+
+private extension DayForecastCell {
+    
+    enum LayoutConstants {
+        static let superViewCornerRadius: CGFloat = 15.0
+        static let imageViewSize: CGFloat = 28.0
+        static let stackViewSpacing: CGFloat = 5.0
+    }
+    
+    enum Fonts {
+        static let timeLabel: UIFont = .boldSystemFont(ofSize: 14)
+        static let tempLabel: UIFont = .boldSystemFont(ofSize: 14)
+    }
+    
+    enum StringConstants {
+        static let cellIdentifier: String = "DayForecastCell"
     }
     
 }
