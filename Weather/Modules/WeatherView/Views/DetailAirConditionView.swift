19cd7362fee480dcdea36391c1626c5d194745a3
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2026-04-03 23:34:58 +0300

    DetailAirCondirionView's code refactoring.

--- Weather/Modules/WeatherView/Views/DetailAirConditionView.swift
+++ Weather/Modules/WeatherView/Views/DetailAirConditionView.swift
@@ -10,23 +10,16 @@ import Combine
 
 final class DetailAirConditionView: UICollectionViewController {
     
-    //MARK: - Public fields
+    //MARK: - Public properties
     
     typealias Output = DetailAirConditionDataModel.AirConditionCellOutputData
     
-    enum Section: Int, CaseIterable {
-        case main
-    }
-    
-    //MARK: - Private fields
+    //MARK: - Private properties
     
     private let header: HeaderView
-    
     private let viewModel: DetailAirConditionViewModelProtocol
-    private let headerIdentifier = "HeaderView"
     
     private var dataSource: UICollectionViewDiffableDataSource<Section, Output>!
-    
     private var cancelable = Set<AnyCancellable>()
 
     //MARK: - Initializers
@@ -42,7 +35,7 @@ final class DetailAirConditionView: UICollectionViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Life Cycle
+    //MARK: - Life Cycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,75 +48,10 @@ final class DetailAirConditionView: UICollectionViewController {
         super.viewDidDisappear(animated)
         viewModel.finish()
     }
-
-    //MARK: - Private methods
-    
-    private func configure() {
-        collectionView.register(AirConditionCell.self, forCellWithReuseIdentifier: AirConditionCell.cellReuseIdentifier)
-        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
-        collectionView.backgroundColor = Resources.Colors.backgroundColor
-        collectionView.allowsMultipleSelection = false
-        collectionView.allowsSelection = false
-        
-        collectionView.collectionViewLayout = makeCollectionViewLayout()
-        
-        navigationItem.title = "Air Condition"
-        navigationController?.navigationBar.prefersLargeTitles = false
-    }
-    
-    private func bind() {
-        viewModel.data.subscribe(header.viewModel.data).store(in: &cancelable)
-        
-        viewModel.output.sink { [unowned self] _ in
-            self.collectionView.reloadData()
-        }.store(in: &cancelable)
-    }
-    
-    private func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
-        return .init { index, layoutEnviorement in
-            guard let section = Section(rawValue: index) else { return nil }
-            
-            switch section {
-            case .main:
-                let item = NSCollectionLayoutItem(layoutSize:
-                        .init(widthDimension: .fractionalWidth(0.5),
-                              heightDimension: .fractionalHeight(1.0)))
-                
-                item.contentInsets = .init(top: 0,
-                                           leading: 5,
-                                           bottom: 0,
-                                           trailing: 5)
-                
-                let group = NSCollectionLayoutGroup
-                    .horizontal(layoutSize:
-                            .init(widthDimension: .fractionalWidth(1.0),
-                                  heightDimension: .estimated(125)),
-                                subitems: [item, item])
-                
-                let section = NSCollectionLayoutSection(group: group)
-                
-                let headerSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(350))
-                
-                let header = NSCollectionLayoutBoundarySupplementaryItem(
-                    layoutSize: headerSize,
-                    elementKind: UICollectionView.elementKindSectionHeader,
-                    alignment: .top)
-                
-                section.contentInsets = .init(top: 10, leading: 20, bottom: 10, trailing: 20)
-                section.interGroupSpacing = 20
-                section.boundarySupplementaryItems = [header]
-            
-                return section
-                
-            }
-        }
-    }
     
 }
 
-//MARK: - CollectionViewDataSource & Delegate 
+//MARK: - Extension with CollectionViewDataSource & CollectionViewDelegate implementations
 
 extension DetailAirConditionView {
     
@@ -140,7 +68,9 @@ extension DetailAirConditionView {
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath)
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                         withReuseIdentifier: StringConstants.headerIdentifier,
+                                                                         for: indexPath)
         
         header.frame = headerView.bounds
         headerView.addSubview(header)
@@ -149,3 +79,106 @@ extension DetailAirConditionView {
     }
     
 }
+
+//MARK: - Extension with private methods
+
+private extension DetailAirConditionView {
+    
+    func configure() {
+        collectionView.register(AirConditionCell.self, forCellWithReuseIdentifier: AirConditionCell.cellReuseIdentifier)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: StringConstants.headerIdentifier)
+        collectionView.backgroundColor = Resources.Colors.backgroundColor
+        collectionView.allowsMultipleSelection = false
+        collectionView.allowsSelection = false
+        
+        collectionView.collectionViewLayout = makeCollectionViewLayout()
+        
+        navigationItem.title = StringConstants.navigationViewTitle
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    func bind() {
+        viewModel.data
+            .subscribe(header.viewModel.data)
+            .store(in: &cancelable)
+        
+        viewModel.output.sink { [weak self] _ in
+            self?.collectionView.reloadData()
+        }.store(in: &cancelable)
+    }
+    
+    func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        return .init { index, layoutEnviorement in
+            guard let section = Section(rawValue: index) else { return nil }
+            
+            switch section {
+            case .main:
+                let item = NSCollectionLayoutItem(layoutSize:
+                        .init(widthDimension: .fractionalWidth(LayoutConstants.itemWidth),
+                              heightDimension: .fractionalHeight(LayoutConstants.itemHeight)))
+                
+                item.contentInsets = .init(top: LayoutConstants.itemVerticalContentInset,
+                                           leading: LayoutConstants.itemHorizontalContentInset,
+                                           bottom: LayoutConstants.itemVerticalContentInset,
+                                           trailing: LayoutConstants.itemHorizontalContentInset)
+                
+                let group = NSCollectionLayoutGroup
+                    .horizontal(layoutSize:
+                            .init(widthDimension: .fractionalWidth(LayoutConstants.groupWidth),
+                                  heightDimension: .estimated(LayoutConstants.groupHeight)),
+                                subitems: [item, item])
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                let headerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(LayoutConstants.headerWidth),
+                    heightDimension: .absolute(LayoutConstants.headerHeight))
+                
+                let header = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerSize,
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top)
+                
+                section.contentInsets = .init(top: LayoutConstants.sectionVerticalContentInset,
+                                              leading: LayoutConstants.sectionHorizontalContentInset,
+                                              bottom: LayoutConstants.sectionVerticalContentInset,
+                                              trailing: LayoutConstants.sectionHorizontalContentInset)
+                section.interGroupSpacing = LayoutConstants.interGroupSpacing
+                section.boundarySupplementaryItems = [header]
+            
+                return section
+                
+            }
+        }
+    }
+}
+
+//MARK: - Extension with private subobjects
+
+private extension DetailAirConditionView {
+    
+    enum LayoutConstants {
+        static let itemWidth: CGFloat = 0.5
+        static let itemHeight: CGFloat = 1.0
+        static let itemVerticalContentInset: CGFloat = 0.0
+        static let itemHorizontalContentInset: CGFloat = 5.0
+        static let groupWidth: CGFloat = 1.0
+        static let groupHeight: CGFloat = 125.0
+        static let headerWidth: CGFloat = 1.0
+        static let headerHeight: CGFloat = 350
+        static let sectionVerticalContentInset: CGFloat = 10.0
+        static let sectionHorizontalContentInset: CGFloat = 20.0
+        static let interGroupSpacing: CGFloat = 20.0
+    }
+    
+    enum StringConstants {
+        static let navigationViewTitle: String = "Air condition"
+        static let headerIdentifier: String = "HeaderView"
+    }
+    
+    enum Section: Int, CaseIterable {
+        case main
+    }
+    
+}
+
